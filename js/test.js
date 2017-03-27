634d4bc97d0883df4b44ceb87de64ae6a42e31df
noobiwankenoobi <ross.hazelett@gmail.com>
2017-03-27 02:35:23 -0400

    assignment 10 test

---
+++ js/test.js
@@ -0,0 +1,22 @@
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
